af94df6205e544dcc87d3a107c37be4e09f57916
dsaha007 <143870743+dsaha007@users.noreply.github.com>
2025-05-01 23:33:28 +0530

    Update environment configuration for production and development

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,12 @@
 export const environment = {
   production: true,
   firebase: {
-
+    apiKey: "AIzaSyDYwekl_jghtftmIxZR2O5co4xW3OQizzM",
+    authDomain: "pollwave-f25ca.firebaseapp.com",
+    projectId: "pollwave-f25ca",
+    storageBucket: "pollwave-f25ca.firebasestorage.app",
+    messagingSenderId: "961580668291",
+    appId: "1:961580668291:web:8829dc4f804fae80f392b6",
+    measurementId: "G-NYYQ2Y9DLK"
   }
 };
